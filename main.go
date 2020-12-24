54a4ce4554c652f902dd327bc70a403eb7b5d3a8
Matt Merrill <392778+theothermattm@users.noreply.github.com>
2020-12-24 08:38:27 -0500

    Some more functionality

--- main.go
+++ main.go
@@ -3,11 +3,11 @@ package main
 import (
 	"bufio"
 	"fmt"
+	flag "github.com/ogier/pflag"
+	log "github.com/sirupsen/logrus"
 	"os"
 	"regexp"
 	"strings"
-	log	"github.com/sirupsen/logrus"
-	flag "github.com/ogier/pflag"
 )
 
 type chord struct {
@@ -19,10 +19,12 @@ type chord struct {
 
 var (
 	inputChord   string
-	mappedChords map[string]chord
+	mappedChords map[string][]chord
 )
 
 func main() {
+
+	// some of this taken from https://www.freecodecamp.org/news/writing-command-line-applications-in-go-2bc8c0ace79d/
 	flag.Parse()
 	// if user does not supply flags, print usage
 	// we can clean this up later by putting this into its own function
@@ -34,7 +36,20 @@ func main() {
 	}
 
 	fmt.Printf("Searching chord(s): %s\n", inputChord)
-	fmt.Print(mappedChords[inputChord])
+	//fmt.Print(mappedChords[inputChord])
+	printChords(mappedChords[inputChord])
+}
+
+func printChords(chords []chord) {
+
+	if len(chords) > 1 {
+		fmt.Println("Chord: " + chords[1].name)
+		fmt.Println("Type: " + chords[1].chordType)
+		fmt.Println("Notes: " + chords[1].notes)
+		for _, result := range chords {
+			fmt.Println(result.fingering)
+		}
+	}
 }
 
 func parseChordFile() {
@@ -47,28 +62,27 @@ func parseChordFile() {
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		var text = scanner.Text()
-		// Sample line: `A         or   Amaj      [0 0 2 2 2 0] (Db E  A) : major triad`
+		// Sample line from file:
+		// A         or   Amaj      [0 0 2 2 2 0] (Db E  A) : major triad
 		var re = regexp.MustCompile(`^(.*)\[(.*)\].*\((.*)\).*:(.*)`)
 		result := re.FindAllStringSubmatch(text, -1)
 		// TODO good lord clean this up!
 		for _, m := range result {
-			if m[1] != "" || m[2] != "" {
-					var chordName = m[1]
-					if strings.Index(m[1], "or") > -1 {
-						var reOr = regexp.MustCompile(`^(.*)or(.*)`)
-						reOrResult := reOr.FindAllStringSubmatch(chordName, -1)
-						for _, n := range reOrResult{
-							var chordKey = strings.Trim(strings.ToUpper(n[1]), " ")
-							newChord := chord{name: n[1], fingering: m[2], notes: m[3], chordType: m[4]}
-							log.Debug("Keying new chord with OR: {" + chordKey + "}\n")
-							mappedChords[chordKey] = newChord
-						}
-					} else {
-						var chordKey = strings.Trim(strings.ToUpper(m[1]), " ")
-						log.Debug("Keying new chord: {" + chordKey + "}\n")
-						newChord := chord{name: m[1], fingering: m[2], notes: m[3], chordType: m[4]}
-						mappedChords[chordKey] = newChord
-					}
+			var chordName = m[1]
+			if strings.Index(m[1], "or") > -1 {
+				var reOr = regexp.MustCompile(`^(.*)or(.*)`)
+				reOrResult := reOr.FindAllStringSubmatch(chordName, -1)
+				for _, n := range reOrResult {
+					var chordKey = strings.Trim(strings.ToUpper(n[1]), " ")
+					newChord := chord{name: n[1], fingering: m[2], notes: m[3], chordType: m[4]}
+					log.Debug("Keying new chord with OR: {" + chordKey + "}\n")
+					mappedChords[chordKey] = append(mappedChords[chordKey], newChord)
+				}
+			} else {
+				var chordKey = strings.Trim(strings.ToUpper(m[1]), " ")
+				log.Debug("Keying new chord: {" + chordKey + "}\n")
+				newChord := chord{name: m[1], fingering: m[2], notes: m[3], chordType: m[4]}
+				mappedChords[chordKey] = append(mappedChords[chordKey], newChord)
 			}
 		}
 	}
@@ -83,6 +97,6 @@ func init() {
 	// TODO add a debug flag
 	log.SetLevel(log.DebugLevel)
 	flag.StringVarP(&inputChord, "chord", "c", "", "Search Chords")
-	mappedChords = make(map[string]chord)
+	mappedChords = make(map[string][]chord)
 	parseChordFile()
 }
